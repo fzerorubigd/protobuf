d9b0ffe71ff475c2d4327e404377393b0584c752
Forud Ghafouri <fzerorubigd@gmail.com>
2022-04-15 08:05:24 +0200

    Use Go Proto instead of GoGo

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module github.com/fzerorubigd/protobuf
 go 1.13
 
 require (
-	github.com/gogo/protobuf v1.3.1
-	github.com/lib/pq v1.2.0
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.5.2
+	github.com/lib/pq v1.10.5
+	github.com/stretchr/testify v1.7.1
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/protobuf v1.28.0
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- types/types-extra.go
+++ types/types-extra.go
@@ -8,35 +8,36 @@ import (
 	"time"
 
 	"github.com/lib/pq"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 // Null is this a null?
-func (m *Timestamp) Null() bool {
-	if m == nil {
+func (x *Timestamp) Null() bool {
+	if x == nil {
 		return true
 	}
-	return !m.Valid
+	return !x.Valid
 }
 
 // Timestamp try to return the timestamp
-func (m *Timestamp) Timestamp() time.Time {
-	return time.Unix(m.Unix, 0)
+func (x *Timestamp) Timestamp() time.Time {
+	return time.Unix(x.Unix, 0)
 }
 
 // MarshalJSON  try to marshal it in json
-func (m *Timestamp) MarshalJSON() ([]byte, error) {
-	if m == nil || !m.Valid {
+func (x *Timestamp) MarshalJSON() ([]byte, error) {
+	if x == nil || !x.Valid {
 		return []byte("null"), nil
 	}
 
-	t := time.Unix(m.GetUnix(), 0)
+	t := time.Unix(x.GetUnix(), 0)
 	return json.Marshal(t)
 }
 
 // UnmarshalJSON try to unmarshal this from a json string
-func (m *Timestamp) UnmarshalJSON(src []byte) error {
+func (x *Timestamp) UnmarshalJSON(src []byte) error {
 	if string(src) == "null" {
-		m.Valid, m.Unix = false, 0
+		x.Valid, x.Unix = false, 0
 		return nil
 	}
 
@@ -46,30 +47,30 @@ func (m *Timestamp) UnmarshalJSON(src []byte) error {
 		return err
 	}
 
-	m.Valid, m.Unix = true, ts.Unix()
+	x.Valid, x.Unix = true, ts.Unix()
 	return nil
 }
 
 // Scan convert the json array into string slice
-func (m *Timestamp) Scan(src interface{}) error {
+func (x *Timestamp) Scan(src interface{}) error {
 	var p pq.NullTime
 	if err := p.Scan(src); err != nil {
 		return err
 	}
 
-	m.Valid, m.Unix = p.Valid, p.Time.Unix()
+	x.Valid, x.Unix = p.Valid, p.Time.Unix()
 	return nil
 }
 
 // Value try to get the string slice representation in database
-func (m *Timestamp) Value() (driver.Value, error) {
-	if m == nil {
+func (x *Timestamp) Value() (driver.Value, error) {
+	if x == nil {
 		return nil, nil
 	}
-	if !m.Valid {
+	if !x.Valid {
 		return nil, nil
 	}
-	return time.Unix(m.Unix, 0), nil
+	return time.Unix(x.Unix, 0), nil
 }
 
 func TimestampProto(ts time.Time) *Timestamp {
@@ -80,26 +81,26 @@ func TimestampProto(ts time.Time) *Timestamp {
 }
 
 // MarshalJSON  try to marshal it in json
-func (m *JSONArray) MarshalJSON() ([]byte, error) {
-	if m == nil || m.Data == nil {
+func (x *JSONArray) MarshalJSON() ([]byte, error) {
+	if x == nil || x.Data == nil {
 		return []byte("null"), nil
 	}
 
-	return json.Marshal(m.Data)
+	return json.Marshal(x.Data)
 }
 
 // UnmarshalJSON try to unmarshal this from a json string
-func (m *JSONArray) UnmarshalJSON(src []byte) error {
+func (x *JSONArray) UnmarshalJSON(src []byte) error {
 	if string(src) == "null" {
-		m.Data = nil
+		x.Data = nil
 		return nil
 	}
 
-	return json.Unmarshal(src, &m.Data)
+	return json.Unmarshal(src, &x.Data)
 }
 
 // Scan convert the json array into string slice
-func (m *JSONArray) Scan(src interface{}) error {
+func (x *JSONArray) Scan(src interface{}) error {
 	var b []byte
 	switch t := src.(type) {
 	case []byte:
@@ -111,49 +112,49 @@ func (m *JSONArray) Scan(src interface{}) error {
 		return fmt.Errorf("unsupported type: %T", src)
 	}
 	if b == nil || string(b) == "null" {
-		m.Data = nil
+		x.Data = nil
 		return nil
 	}
 
-	return (*pq.StringArray)(&m.Data).Scan(src)
+	return (*pq.StringArray)(&x.Data).Scan(src)
 }
 
 // Value try to get the string slice representation in database
-func (m *JSONArray) Value() (driver.Value, error) {
-	if m == nil || m.Data == nil {
+func (x *JSONArray) Value() (driver.Value, error) {
+	if x == nil || x.Data == nil {
 		return nil, nil
 	}
 
-	return (*pq.StringArray)(&m.Data).Value()
+	return (*pq.StringArray)(&x.Data).Value()
 }
 
 // Null return true if the field is null
-func (m *JSONArray) Null() bool {
-	return m == nil || m.Data == nil
+func (x *JSONArray) Null() bool {
+	return x == nil || x.Data == nil
 }
 
 // MarshalJSON  try to marshal it in json
-func (m *JSONMap) MarshalJSON() ([]byte, error) {
-	if m == nil || m.Data == nil {
+func (x *JSONMap) MarshalJSON() ([]byte, error) {
+	if x == nil || x.Data == nil {
 		return []byte("null"), nil
 	}
 
-	return json.Marshal(m.Data)
+	return json.Marshal(x.Data)
 }
 
 // UnmarshalJSON try to unmarshal this from a json string
-func (m *JSONMap) UnmarshalJSON(src []byte) error {
+func (x *JSONMap) UnmarshalJSON(src []byte) error {
 	if string(src) == "null" {
-		m.Data = nil
+		x.Data = nil
 		return nil
 	}
 
-	m.Data = make(map[string]string)
-	return json.Unmarshal(src, &m.Data)
+	x.Data = make(map[string]string)
+	return json.Unmarshal(src, &x.Data)
 }
 
 // Scan convert the json array into string slice
-func (m *JSONMap) Scan(src interface{}) error {
+func (x *JSONMap) Scan(src interface{}) error {
 	var b []byte
 	switch t := src.(type) {
 	case []byte:
@@ -165,105 +166,174 @@ func (m *JSONMap) Scan(src interface{}) error {
 		return fmt.Errorf("unsupported type: %T", src)
 	}
 
-	return m.UnmarshalJSON(b)
+	return x.UnmarshalJSON(b)
 }
 
 // Value try to get the string slice representation in database
-func (m *JSONMap) Value() (driver.Value, error) {
-	if m == nil || m.Data == nil {
+func (x *JSONMap) Value() (driver.Value, error) {
+	if x == nil || x.Data == nil {
 		return nil, nil
 	}
 
-	return m.MarshalJSON()
+	return x.MarshalJSON()
 }
 
 // Null return true if the field is null
-func (m *JSONMap) Null() bool {
-	return m == nil || m.Data == nil
+func (x *JSONMap) Null() bool {
+	return x == nil || x.Data == nil
 }
 
 // MarshalJSON  try to marshal it in json
-func (m *NullString) MarshalJSON() ([]byte, error) {
-	if m == nil || !m.Valid {
+func (x *NullString) MarshalJSON() ([]byte, error) {
+	if x == nil || !x.Valid {
 		return []byte("null"), nil
 	}
 
-	return json.Marshal(m.String_)
+	return json.Marshal(x.String_)
 }
 
 // UnmarshalJSON try to unmarshal this from a json string
-func (m *NullString) UnmarshalJSON(src []byte) error {
+func (x *NullString) UnmarshalJSON(src []byte) error {
 	if string(src) == "null" {
-		m.Valid = false
+		x.Valid = false
 		return nil
 	}
 
-	m.Valid = true
-	return json.Unmarshal(src, &m.String_)
+	x.Valid = true
+	return json.Unmarshal(src, &x.String_)
 }
 
 // Scan convert the json array into string slice
-func (m *NullString) Scan(src interface{}) error {
+func (x *NullString) Scan(src interface{}) error {
 	var b sql.NullString
 	if err := b.Scan(src); err != nil {
 		return err
 	}
-	m.Valid, m.String_ = b.Valid, b.String
+	x.Valid, x.String_ = b.Valid, b.String
 	return nil
 }
 
 // Value try to get the string slice representation in database
-func (m *NullString) Value() (driver.Value, error) {
-	if m == nil || !m.Valid {
+func (x *NullString) Value() (driver.Value, error) {
+	if x == nil || !x.Valid {
 		return nil, nil
 	}
-	return m.String_, nil
+	return x.String_, nil
 }
 
 // Null return true if the field is null
-func (m *NullString) Null() bool {
-	return m == nil || !m.Valid
+func (x *NullString) Null() bool {
+	return x == nil || !x.Valid
 }
 
 // MarshalJSON  try to marshal it in json
-func (m *NullInt64) MarshalJSON() ([]byte, error) {
-	if m == nil || !m.Valid {
+func (x *NullInt64) MarshalJSON() ([]byte, error) {
+	if x == nil || !x.Valid {
 		return []byte("null"), nil
 	}
 
-	return json.Marshal(m.Int64)
+	return json.Marshal(x.Int64)
 }
 
 // UnmarshalJSON try to unmarshal this from a json string
-func (m *NullInt64) UnmarshalJSON(src []byte) error {
+func (x *NullInt64) UnmarshalJSON(src []byte) error {
 	if string(src) == "null" {
-		m.Valid = false
+		x.Valid = false
 		return nil
 	}
 
-	m.Valid = true
-	return json.Unmarshal(src, &m.Int64)
+	x.Valid = true
+	return json.Unmarshal(src, &x.Int64)
 }
 
 // Scan convert the json array into string slice
-func (m *NullInt64) Scan(src interface{}) error {
+func (x *NullInt64) Scan(src interface{}) error {
 	var b sql.NullInt64
 	if err := b.Scan(src); err != nil {
 		return err
 	}
-	m.Valid, m.Int64 = b.Valid, b.Int64
+	x.Valid, x.Int64 = b.Valid, b.Int64
 	return nil
 }
 
 // Value try to get the string slice representation in database
-func (m *NullInt64) Value() (driver.Value, error) {
-	if m == nil || !m.Valid {
+func (x *NullInt64) Value() (driver.Value, error) {
+	if x == nil || !x.Valid {
 		return nil, nil
 	}
-	return m.Int64, nil
+	return x.Int64, nil
 }
 
 // Null return true if the field is null
-func (m *NullInt64) Null() bool {
-	return m == nil || !m.Valid
+func (x *NullInt64) Null() bool {
+	return x == nil || !x.Valid
+}
+
+// MarshalJSON  try to marshal it in json
+func (x *JSONObject) MarshalJSON() ([]byte, error) {
+	if x == nil || x.Data == nil {
+		return []byte("null"), nil
+	}
+
+	return json.Marshal(x.Data.AsMap())
+}
+
+// UnmarshalJSON try to unmarshal this from a json string
+func (x *JSONObject) UnmarshalJSON(src []byte) error {
+	if string(src) == "null" {
+		x.Data = nil
+		return nil
+	}
+
+	var iface map[string]interface{}
+	if err := json.Unmarshal(src, &iface); err != nil {
+		return err
+	}
+
+	var err error
+	x.Data, err = structpb.NewStruct(iface)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Scan convert the json array into string slice
+func (x *JSONObject) Scan(src interface{}) error {
+	var b []byte
+	switch t := src.(type) {
+	case []byte:
+		b = t
+	case string:
+		b = []byte(t)
+	case nil:
+	default:
+		return fmt.Errorf("unsupported type: %T", src)
+	}
+
+	return x.UnmarshalJSON(b)
+}
+
+// Value try to get the string slice representation in database
+func (x *JSONObject) Value() (driver.Value, error) {
+	if x == nil || x.Data == nil {
+		return nil, nil
+	}
+
+	return x.MarshalJSON()
+}
+
+// Null return true if the field is null
+func (x *JSONObject) Null() bool {
+	return x == nil || x.Data == nil
+}
+
+// NewJSONObject create a new JSONObject from a map
+func NewJSONObject(data map[string]interface{}) (*JSONObject, error) {
+	d, err := structpb.NewStruct(data)
+	if err != nil {
+		return nil, err
+	}
+	return &JSONObject{Data: d}, nil
 }
